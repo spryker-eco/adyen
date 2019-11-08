c06ef3e59a29b6725eda933dbefd94a7d48c4978
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-11-08 16:53:05 +0200

    ECO-3287. [Adyen] Duplicate Adyen Merchant reference
    * Updated `config_default.php`.
    * Updated `config.dist.php` in the Adyen module.
    * Updated `AdyenStub::handleCreditCard3dResponseFromAdyen()`.

--- config/config.dist.php
+++ config/config.dist.php
@@ -46,15 +46,15 @@ $config[AdyenConstants::IDEAL_ISSUERS_LIST] = [
 ];
 
 $config[AdyenApiConstants::API_KEY] = 'PUT YOUR API KEY HERE';
-$config[AdyenApiConstants::GET_PAYMENT_METHODS_ACTION_URL] = 'https://checkout-test.adyen.com/checkout/V51/paymentMethods';
-$config[AdyenApiConstants::MAKE_PAYMENT_ACTION_URL] = 'https://checkout-test.adyen.com/checkout/V51/payments';
-$config[AdyenApiConstants::PAYMENTS_DETAILS_ACTION_URL] = 'https://checkout-test.adyen.com/checkout/V51/payments/details';
-$config[AdyenApiConstants::AUTHORIZE_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/V51/authorise';
-$config[AdyenApiConstants::AUTHORIZE_3D_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/V51/authorise3d';
-$config[AdyenApiConstants::CAPTURE_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/V51/capture';
-$config[AdyenApiConstants::CANCEL_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/V51/cancel';
-$config[AdyenApiConstants::REFUND_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/V51/refund';
-$config[AdyenApiConstants::CANCEL_OR_REFUND_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/V51/cancelOrRefund';
+$config[AdyenApiConstants::GET_PAYMENT_METHODS_ACTION_URL] = 'https://checkout-test.adyen.com/checkout/v50/paymentMethods';
+$config[AdyenApiConstants::MAKE_PAYMENT_ACTION_URL] = 'https://checkout-test.adyen.com/checkout/v50/payments';
+$config[AdyenApiConstants::PAYMENTS_DETAILS_ACTION_URL] = 'https://checkout-test.adyen.com/checkout/v50/payments/details';
+$config[AdyenApiConstants::AUTHORIZE_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/v50/authorise';
+$config[AdyenApiConstants::AUTHORIZE_3D_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/v50/authorise3d';
+$config[AdyenApiConstants::CAPTURE_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/v50/capture';
+$config[AdyenApiConstants::CANCEL_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/v50/cancel';
+$config[AdyenApiConstants::REFUND_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/v50/refund';
+$config[AdyenApiConstants::CANCEL_OR_REFUND_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/v50/cancelOrRefund';
 $config[AdyenApiConstants::TECHNICAL_CANCEL_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/v50/technicalCancel';
 $config[AdyenApiConstants::ADJUST_AUTHORIZATION_ACTION_URL] = 'https://pal-test.adyen.com/pal/servlet/Payment/v50/adjustAuthorisation';
 
