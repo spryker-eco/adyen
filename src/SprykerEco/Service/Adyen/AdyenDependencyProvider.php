56f26c1f4c43351ae65c1d34518db902a7f10ed3
Denis Gnusov <denis.gnusov@spryker.com>
2020-07-07 17:15:28 +0300

    ECO-3544 Fixed CI issues

--- src/SprykerEco/Service/Adyen/AdyenDependencyProvider.php
+++ src/SprykerEco/Service/Adyen/AdyenDependencyProvider.php
@@ -35,9 +35,9 @@ class AdyenDependencyProvider extends AbstractBundleDependencyProvider
      */
     protected function addUtilTextService(Container $container): Container
     {
-        $container[static::SERVICE_UTIL_TEXT] = function (Container $container) {
+        $container->set(static::SERVICE_UTIL_TEXT, function (Container $container) {
             return new AdyenToUtilTextServiceBridge($container->getLocator()->utilText()->service());
-        };
+        });
 
         return $container;
     }
