deccdc2ffb967dec0d93a04eab64fc3abfd08dcd
maslov <dev.maslov.a@gmail.com>
2020-07-16 18:21:41 +0300

    TE-6081: Introduced webpackChunkName commentary.

--- src/SprykerEco/Yves/Adyen/Theme/default/components/molecules/adyen-credit-card/index.ts
+++ src/SprykerEco/Yves/Adyen/Theme/default/components/molecules/adyen-credit-card/index.ts
@@ -1,4 +1,6 @@
 import './adyen-credit-card.scss';
-
 import register from 'ShopUi/app/registry';
-export default register('adyen-credit-card', () => import(/* webpackMode: "lazy" */'./adyen-credit-card'));
+export default register('adyen-credit-card', () => import(
+    /* webpackMode: "lazy" */
+    /* webpackChunkName: "adyen-credit-card" */
+    './adyen-credit-card'));
