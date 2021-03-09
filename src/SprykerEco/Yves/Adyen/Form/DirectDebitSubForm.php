02641950e288de327a5605a55a42a9093e408506
boykodev <boykodev@gmail.com>
2021-03-09 17:46:20 +0200

    fix direct debit sub form

--- src/SprykerEco/Yves/Adyen/Form/DirectDebitSubForm.php
+++ src/SprykerEco/Yves/Adyen/Form/DirectDebitSubForm.php
@@ -92,7 +92,9 @@ class DirectDebitSubForm extends AbstractSubForm
                 'label' => static::OWNER_NAME_LABEL,
                 'required' => true,
                 'constraints' => [
-                    $this->createNotBlankConstraint(static::GLOSSARY_KEY_CONSTRAINT_MESSAGE_INVALID_OWNER_NAME),
+                    $this->createNotBlankConstraint([
+                        'message' => static::GLOSSARY_KEY_CONSTRAINT_MESSAGE_INVALID_OWNER_NAME,
+                    ]),
                 ],
             ]
         );
@@ -114,7 +116,9 @@ class DirectDebitSubForm extends AbstractSubForm
                 'label' => static::IBAN_NUMBER_LABEL,
                 'required' => true,
                 'constraints' => [
-                    $this->createNotBlankConstraint(static::GLOSSARY_KEY_CONSTRAINT_MESSAGE_INVALID_IBAN_NUMBER),
+                    $this->createNotBlankConstraint([
+                        'message' => static::GLOSSARY_KEY_CONSTRAINT_MESSAGE_INVALID_IBAN_NUMBER,
+                    ]),
                 ],
             ]
         );
