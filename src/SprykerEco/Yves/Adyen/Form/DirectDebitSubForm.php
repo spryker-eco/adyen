b80a4d3a14f29583960d09dcfa50fd5dfcd0fe8a
Michael Knappe <michael.knappe@spryker.com>
2021-10-08 15:46:37 +0200

    Fixed phpstan error messages

--- src/SprykerEco/Yves/Adyen/Form/DirectDebitSubForm.php
+++ src/SprykerEco/Yves/Adyen/Form/DirectDebitSubForm.php
@@ -115,7 +115,7 @@ class DirectDebitSubForm extends AbstractSubForm
                 'label' => static::OWNER_NAME_LABEL,
                 'required' => true,
                 'constraints' => [
-                    $this->createNotBlankConstraint(static::GLOSSARY_KEY_CONSTRAINT_MESSAGE_INVALID_OWNER_NAME),
+                    $this->createNotBlankConstraint([static::GLOSSARY_KEY_CONSTRAINT_MESSAGE_INVALID_OWNER_NAME]),
                 ],
             ]
         );
@@ -137,7 +137,7 @@ class DirectDebitSubForm extends AbstractSubForm
                 'label' => static::IBAN_NUMBER_LABEL,
                 'required' => true,
                 'constraints' => [
-                    $this->createNotBlankConstraint(static::GLOSSARY_KEY_CONSTRAINT_MESSAGE_INVALID_IBAN_NUMBER),
+                    $this->createNotBlankConstraint([static::GLOSSARY_KEY_CONSTRAINT_MESSAGE_INVALID_IBAN_NUMBER]),
                 ],
             ]
         );
